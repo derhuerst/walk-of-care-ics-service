142888b76ae538092a0dc0527ba95e56090de0fd
Jannis R <mail@jannisr.de>
2020-09-22 13:15:46 +0200

    add HTTP server

---
+++ api/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const aboutRoute = require('ics-service/about')
+
+module.exports = aboutRoute(
+	'Critical Mass Berlin calendar feed',
+	'/api/feed',
+)
--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict'
 
-// todo
+const {createServer} = require('http')
+const about = require('./api')
+const feed = require('./api/feed')
+
+const server = createServer((req, res) => {
+	const path = new URL(req.url, 'http://example.org').pathname
+
+	if (path === '/') {
+		about(req, res)
+	} else if (path === '/api/feed') {
+		feed(req, res)
+	} else {
+		res.writeHead(404, 'not found')
+		res.end('not found')
+	}
+})
+
+const port = parseInt(process.env.PORT || 3000)
+server.listen(port, (err) => {
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	} else console.info('listening on ' + port)
+})
